2fa3425a8a77c160a2f2d6b79019e47796c13da4
shigeru <hironoki3@gmail.com>
2020-12-14 09:22:54 +0900

    first commit.

---
+++ src/test/java/TestGeneratePrimes.java
@@ -0,0 +1,29 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+public class TestGeneratePrimes extends TestCase {
+	public static void main(String args[]) {
+		junit.swingui.TestRunner.main(
+				new String[] {"TestGeneratePrimes"}
+		);
+	}
+
+	public TestGeneratePrimes(String name) {
+		super(name);
+	}
+
+	public void testPrimes() {
+		int[] nullArray = GeneratePrimes.generatePrimes(0);
+		assertEquals(nullArray.length, 0);
+		int[] minArray = GeneratePrimes.generatePrimes(2);
+		assertEquals(minArray.length, 1);
+		assertEquals(minArray[0], 2);
+		int[] threeArray = GeneratePrimes.generatePrimes(3);
+		assertEquals(threeArray.length, 2);
+		assertEquals(threeArray[0], 2);
+		assertEquals(threeArray[1], 3);
+		int[] centArray = GeneratePrimes.generatePrimes(100);
+		assertEquals(centArray.length, 25);
+		assertEquals(centArray[24], 97);
+	}
+}
